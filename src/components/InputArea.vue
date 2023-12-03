9e0d277a0be93ac8f6f88ddaf6bbb7cb25188dc9
Lucas Zambam <lucaszambam7896@gmail.com>
2023-12-03 09:37:08 -0300

    implement input behavior

--- src/components/InputArea.vue
+++ src/components/InputArea.vue
@@ -1,17 +1,39 @@
 <template>
 	<div class="input-area">
-		<div class="input-title-container" style="width: 15%;">
-			<input id="guessTitle" name="guessTitle" type="text" placeholder="Digite o título do artigo">
-		</div>
-		<div class="input-word-container">
-			<input id="guessWord" name="guessWord" type="text" placeholder="Palpite uma palavra">
+		<div class="input-container input-title-container">
+			<input id="guessTitle" name="guessTitle" type="text" required placeholder="Digite o título do artigo" @keyup.enter="submitGuess($event)" data-input-type="title">
+            <button class="submit-container" data-input-type="title" @click="onClickSubmitGuess"><img height="25" width="25" src="@/assets/send.svg" alt="Send"></button>
+		</div> 
+		<div class="input-container input-word-container">
+			<input id="guessWord" name="guessWord" type="text" required placeholder="Palpite uma palavra" @keyup.enter="submitGuess($event)" data-input-type="word">
+            <button class="submit-container" data-input-type="word" @click="onClickSubmitGuess"><img height="25" width="25" src="@/assets/send.svg" alt="Send"></button>
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'InputArea'
+	name: 'InputArea',
+    emits: ['wordGuess', 'titleGuess'],
+    methods: {
+        submitGuess(event) {
+            this.$emit(`${event.target.dataset.inputType}Guess`, event.target.value);
+            event.target.value = '';
+        },
+
+        onClickSubmitGuess(event) {
+            const inputElement = document.querySelector(`input[data-input-type="${event.currentTarget.dataset.inputType}"]`);
+            if (inputElement) {
+                inputElement.dispatchEvent(new KeyboardEvent('keyup', {
+                    key: 'Enter',
+                    code: 'Enter',
+                    keyCode: 13,
+                    which: 13,
+                    bubbles: true
+                }));
+            }
+        }
+    }
 }
 </script>
 
@@ -26,20 +48,36 @@ export default {
     height: 75px;
     width: -webkit-fill-available;
 	gap: 50px;
-}
 
-input {
-	width: 100%;
-    height: 20px;
-    padding: 5px;
-    background-color: #21262d;
-    color: #fff;
-    border: 1px solid #7d8590;
-    border-radius: 0.4rem;
-    font-weight: 700;
-    font-family: monospace;
-    font-size: 1rem;
-    -moz-text-align-last: center;
-    text-align-last: center;
+    .input-container {
+        display: flex;
+        gap: 0.5rem;
+        justify-content: center;
+        align-items: center;
+        &.input-title-container {
+            min-width: 290px;
+        }
+
+        .submit-container {
+            display: flex;
+            background: none;
+            border: 0;
+        }
+
+        input {
+            width: 100%;
+            height: 20px;
+            padding: 5px;
+            background-color: #21262d;
+            color: #fff;
+            border: 1px solid #7d8590;
+            border-radius: 0.4rem;
+            font-weight: 700;
+            font-family: monospace;
+            font-size: 1rem;
+            -moz-text-align-last: center;
+            text-align-last: center;
+        }
+    }
 }
 </style>
